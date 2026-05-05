25b9c34fb6a72d748cf234576ff29bb26ff84a42
Dariush Moshiri <dariush.moshiri@outlook.com>
2026-05-05 11:04:50 -0400

    feat: minimal cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+mod target;
+
+use clap::{Parser, Subcommand};
+#[derive(Parser, Debug)]
+#[command(name="isdown")]
+#[command(about="Detect downtime on any service", long_about="")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands, 
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Check {
+        #[arg(required = true)]
+        targets: Vec<String>
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Check { targets } => {
+            for target in targets.iter() {
+                println!("{}", target);
+            }
+        }
+    }
 }
---
+++ src/target.rs
@@ -0,0 +1,4 @@
+
+enum Target {
+    Provider(String), // TODO: It should contain the details from a registry
+}
